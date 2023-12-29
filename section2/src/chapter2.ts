6436845a7fd3aef31d418dac2bc0a947c1d2eaa8
kkibum22 <950610m@naver.com>
2023-12-29 15:27:19 +0900

    📝 배열,튜플

---
+++ section2/src/chapter2.ts
@@ -0,0 +1,30 @@
+//배열
+let numArr: number[] = [1, 2, 3];
+let strArr: string[] = ["hi", "bye", "happy"];
+//제네릭 문법 추후에 배움 let booArr: Array<boolean> = [true, false, true];
+
+//배열의 들어가는 요쇼의 타입이 다양할 경우
+let multiArr: (number | string)[] = [1, "Hello"];
+
+//다차원 배열의 타입을 정의하는 방법
+let doubleArr: number[][] = [
+  [1, 2, 3],
+  [4, 5],
+];
+
+//튜플 ->길이,타입이 고정된 배열
+let tup1: [number, number] = [1, 2];
+tup1 = [1, 2, 3]; //길이
+tup1 = ["1", "2"]; //타입
+
+let tup2: [number, string, boolean] = [1, "2", true];
+//순서에 맞게 맞는 타입이 들어가야됨
+
+//튜플을 사용하는 예
+const users: [string, number][] = [
+  ["a", 1],
+  ["b", 2],
+  ["c", 3],
+  ["d", 4],
+  [5, "dd"],
+];
